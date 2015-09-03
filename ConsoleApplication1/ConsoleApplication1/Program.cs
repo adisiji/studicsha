140b2eafe3e263dc0c960f5d55455953eb43ed42
adisiji <madidarma1@gmail.com>
2015-09-03 17:54:18 +0700

    pertama di lepkom

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 10;
+            int c = b - a;
+            string u;
+            Console.WriteLine("Haloo, nilai c = {0} dan b = {1}", c, b);
+            Console.WriteLine("Nilai Modulus {0} mod {1} = {2}", b, a, b % a);
+            Console.Write("Masukkan umur Anda : ");
+            u = Console.ReadLine();
+            Console.WriteLine("jadi umur anda {0} tahun yaa", u);
+            if (a > b)
+            { Console.WriteLine("a({0}) lebih besar dari b({1})", a, b); }
+            else if (b > a)
+            { Console.WriteLine("b({0}) lebih besar dari a({1})", b, a); }
+            else if (b == a)
+            { Console.WriteLine("a({0}) sama dengan b({1})", a, b);      }
+            switch (a)
+            {
+                case 5:
+                    Console.WriteLine("Lima");
+                    break;
+                case 10:
+                    Console.WriteLine("Sepuluh");
+                    break;
+                case 8:
+                    Console.WriteLine("delapan");
+                    break;
+                default:
+                    Console.WriteLine("not found..");
+                    break;
+            }
+
+        }
+
+    }
+}
---
+++ buatobjek/buatobjek/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buatobjek
+{
+    class luas
+    {
+        private int panjang;
+        public luas(int p) {
+            panjang = p;
+        }
+        public void setLength(int len)
+        {
+            panjang = len;
+        }
+        public double getLength()
+        {
+            return panjang;
+        }
+
+        static void Main(string[] args)
+        {
+            luas aseli = new luas(10);
+            Console.WriteLine("Isi luas adalah {0}", aseli.getLength());
+            aseli.setLength(67);
+            Console.WriteLine("Isi luas jadi {0}", aseli.getLength());
+        }
+    }
+}
